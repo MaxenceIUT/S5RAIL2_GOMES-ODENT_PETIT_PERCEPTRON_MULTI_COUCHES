08f5db2d85dcf4d3bd15655191dd532f0d9d894f
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-12-08 16:43:30 +0100

    feat: added sigmoid transfer function

---
+++ src/Sigmoide.java
@@ -0,0 +1,15 @@
+import framework.TransferFunction;
+
+public class Sigmoide implements TransferFunction {
+
+    @Override
+    public double evaluate(double value) {
+        return 1 / (1 + Math.exp(-value));
+    }
+
+    @Override
+    public double evaluateDer(double value) {
+        return evaluate(value) - Math.pow(evaluate(value), 2);
+    }
+
+}
