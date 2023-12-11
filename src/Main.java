244c6e8538deebef9e666ef44a608259b69470d8
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-12-11 10:41:39 +0100

    refactor: using Configuration class and getting parameters from command line

--- src/Main.java
+++ src/Main.java
@@ -9,41 +9,57 @@ import java.util.Random;
 public class Main {
 
     private static final int MAX_ITERATIONS = 1_000_000;
-    private static final double[][] INPUT = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
-    private static final double[][] AND_OUTPUT = {{0}, {0}, {0}, {1}};
-    private static final double[][] OR_OUTPUT = {{0}, {1}, {1}, {1}};
-    private static final double[][] XOR_OUTPUT = {{0}, {1}, {1}, {0}};
 
     public static void main(String[] args) {
-        int[] layers = {2, 2, 1};
-        double learningRate = 0.6;
-        double errorTarget = 0.01;
-        TransferFunction transferFunction = new Sigmoide();
-
-        MLP mlp = new MLP(layers, learningRate, transferFunction);
+        Configuration configuration = Configuration.fromArgs(args);
         Random random = new Random();
 
+        // Récupération des paramètres depuis la configuration
+        int[] layers = configuration.getLayers();
+        double learningRate = configuration.getLearningRate();
+        double errorTarget = configuration.getErrorTarget();
+        TransferFunction transferFunction = configuration.getTransferFunction();
+        DataSet dataSet = configuration.getDataSet();
+
+        // On affiche les paramètres utilisés
+        StringBuilder params = new StringBuilder();
+        params.append("-".repeat(50)).append("\n");
+        params.append("Paramètres :").append("\n");
+        params.append("\t").append("- Pas d'apprentissage : ").append(learningRate).append("\n");
+        params.append("\t").append("- Taux d'erreur cible : ").append(errorTarget).append("\n");
+        params.append("\t").append("- Fonction de transfert : ").append(transferFunction.getClass().getSimpleName()).append("\n");
+        params.append("\t").append("- Couches : ").append(Arrays.toString(layers)).append("\n");
+        params.append("\t").append("- Jeu de données : ").append(dataSet.name()).append("\n");
+        params.append("-".repeat(50));
+        System.out.println(params);
+
+        // On instancie le perceptron multicouche
+        MLP mlp = new MLP(layers, learningRate, transferFunction);
+
+        // On démarre l'apprentissage
         Instant start = Instant.now();
         System.out.println("Apprentissage en cours...");
 
-        // Création d'un tableau rempli de 1 (taille INPUT.length)
-        double[] trainingError = Arrays.stream(INPUT).mapToDouble(input -> 1.0).toArray();
+        // Création d'un tableau rempli de 1 (taille dataSet.getInputsArray().length)
+        double[] trainingError = Arrays.stream(dataSet.getOutputsArray()).mapToDouble(input -> 1.0).toArray();
         int i = 0;
         // Tant que l'on a pas excédé le nombre d'itérations max et qu'au moins le taux d'erreur d'un exemple est supérieur à l'erreur cible
         while (i < MAX_ITERATIONS && Arrays.stream(trainingError).anyMatch(error -> error > errorTarget)) {
-            int randomInput = random.nextInt(INPUT.length);
-            double[] input = INPUT[randomInput];
-            double[] output = OR_OUTPUT[randomInput];
+            int randomInput = random.nextInt(dataSet.getInputsArray().length);
+            double[] input = dataSet.getInput(randomInput);
+            double[] output = dataSet.getOutput(randomInput);
 
             trainingError[randomInput] = mlp.backPropagate(input, output);
             i++;
         }
 
+        // Fin de l'apprentissage
         Instant end = Instant.now();
 
         Duration duration = Duration.between(start, end);
         String ms = duration.toMillis() + "ms";
 
+        // On affiche les statistiques
         StringBuilder sb = new StringBuilder();
         sb.append("-".repeat(50)).append("\n");
         sb.append("Apprentissage terminé en ").append(ms).append("\n");
@@ -53,12 +69,13 @@ public class Main {
             sb.append("Erreur finale moyenne : ").append(value).append("\n");
         });
         sb.append("-".repeat(50));
+        System.out.println(sb);
 
-        for (double[] doubles : OR_OUTPUT) {
-            System.out.println(Arrays.toString(doubles) + " -> " + Arrays.toString(mlp.execute(doubles)));
+        System.out.println("\nTest des exemples: ");
+        for (double[] doubles : dataSet.getInputsArray()) {
+            System.out.println(" - " + Arrays.toString(doubles) + " -> " + Arrays.toString(mlp.execute(doubles)));
         }
 
-        System.out.println(sb);
     }
 
 }
