cdb1216878acd4cb7cc88f28e5365b7942b591a6
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-12-20 16:42:00 +0100

    refactor: MLP doesn't try to reach an errorTarget, but rather runs for the given iteration count. Added statistics collection, comments, and degressive learning rate

--- src/classification/MLPClassification.java
+++ src/classification/MLPClassification.java
@@ -11,28 +11,43 @@ import java.util.List;
 
 public class MLPClassification extends AlgoClassification {
 
-    private final MLP mlp;
+    private MLP mlp;
+    private final int trainingIterationsCount;
+    private final List<Imagette> testImagettes;
+    private final int[] middleLayers;
+    private final double initialLearningRate;
+    private final double finalLearningRate;
+    private final TransferFunction transferFunction;
 
-    public MLPClassification(List<Imagette> trainingImagettes, List<Imagette> testImagettes, int[] middleLayers,
-                             double learningRate, TransferFunction transferFunction, double errorTarget) {
+    public MLPClassification(List<Imagette> trainingImagettes, int trainingIterationsCount, List<Imagette> testImagettes, int[] middleLayers, double initialLearningRate, double finalLearningRate, TransferFunction transferFunction) {
         super(trainingImagettes);
+        this.trainingIterationsCount = trainingIterationsCount;
+        this.testImagettes = testImagettes;
+        this.middleLayers = middleLayers;
+        this.initialLearningRate = initialLearningRate;
+        this.finalLearningRate = finalLearningRate;
+        this.transferFunction = transferFunction;
+    }
 
+    public LearningStatistics train() {
+        LearningStatistics stats = new LearningStatistics(middleLayers);
+
+        // On récupère le nombre de pixels d'une imagette
         Imagette first = trainingImagettes.getFirst();
         int pixels = first.getRows() * first.getCols();
 
-        int[] layers = new int[middleLayers.length + 2];
-        layers[0] = pixels;
-        System.arraycopy(middleLayers, 0, layers, 1, middleLayers.length);
-        layers[layers.length - 1] = 10;
+        // On définit les couches
+        int[] layers = getLayers(pixels, middleLayers);
 
-        this.mlp = new MLP(layers, learningRate, transferFunction);
+        // On crée le MLP
+        this.mlp = new MLP(layers, initialLearningRate, transferFunction);
 
-        double errorPercentage = 100;
-        int maxIterations = 1_000_000;
-        int i = 0;
-
-        while (i < maxIterations && errorPercentage > errorTarget) {
+        // On lance l'apprentissage
+        for (int i = 1; i <= trainingIterationsCount; i++) {
             Progress progress = new Progress("Itération n°" + i + " - Apprentissage... ");
+            double learningFailureRate = 0;
+
+            // On s'entraîne sur toutes les imagettes d'apprentissage 10 fois, en mélangeant les imagettes à chaque fois
             for (int j = 0; j < 10; j++) {
                 progress.setProgress(j, 10);
 
@@ -44,11 +59,15 @@ public class MLPClassification extends AlgoClassification {
                     double[] inputs = getInputsArray(imagette);
                     double[] outputs = new double[10];
                     outputs[imagette.getLabel()] = 1;
-                    this.mlp.backPropagate(inputs, outputs);
+                    learningFailureRate += this.mlp.backPropagate(inputs, outputs);
                     if(k % 100 == 0) progress.setProgress(j + (double) k / training.size(), 10);
                 }
             }
+
+            learningFailureRate /= 10 * trainingImagettes.size();
             progress.setLabel("Itération n°" + i + " - Test... ");
+
+            // Après ces 10 itérations, on calcule notre pourcentage d'erreur sur la base de test (1000 imagettes aléatoires)
 
             Collections.shuffle(testImagettes);
             List<Imagette> shuffledImagette = testImagettes.stream().limit(1000).toList();
@@ -70,11 +89,30 @@ public class MLPClassification extends AlgoClassification {
                 j++;
             }
 
-            errorPercentage = (double) unsatisfyingValues / shuffledImagette.size() * 100;
-            progress.complete("Fin de l'itération n°" + i + " - Pourcentage d'erreur: " + errorPercentage + "%");
+            // On calcule donc le pourcentage d'erreur de reconnaissance
+            double errorPercentage = (double) unsatisfyingValues / shuffledImagette.size() * 100;
+            progress.complete("Fin de l'itération n°" + i + " - Pourcentage d'erreur: " + errorPercentage + "% - " +
+                    "Pourcentage d'erreur sur la base d'apprentissage: " + learningFailureRate * 100 + "%");
 
-            i++;
+            // On génère un objet contenant les statistiques de l'itération
+            IterationStatistics iterationStatistics = new IterationStatistics(i, errorPercentage, learningFailureRate);
+            stats.addIterationStatistics(iterationStatistics);
+
+            // On met à jour le learning rate (qui est dégressif)
+            double slope = (finalLearningRate - initialLearningRate) / trainingIterationsCount;
+            mlp.setLearningRate(initialLearningRate + slope * i);
         }
+
+        // Une fois l'apprentissage terminé, on retourne les statistiques
+        return stats;
+    }
+
+    private int[] getLayers(int pixels, int[] middleLayers) {
+        int[] layers = new int[middleLayers.length + 2];
+        layers[0] = pixels;
+        System.arraycopy(middleLayers, 0, layers, 1, middleLayers.length);
+        layers[layers.length - 1] = 10;
+        return layers;
     }
 
     /**
