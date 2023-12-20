c5c4724a9fdc354c0b7fd3d0a8d8b9b5deb84455
MAXOUXAX <24844231+MAXOUXAX@users.noreply.github.com>
2023-12-20 16:42:39 +0100

    feat: now training 8 MLPs at the same time using multithreading, collecting statistics and saving them to CSV files

--- src/TestsMNIST.java
+++ src/TestsMNIST.java
@@ -4,7 +4,9 @@ import framework.TransferFunction;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 public class TestsMNIST {
 
@@ -12,6 +14,8 @@ public class TestsMNIST {
     public static final String TRAIN_LABELS_IDX1 = "numbers/train-labels.idx1-ubyte";
     public static final String TEST_IMAGES_IDX3 = "numbers/t10k-images.idx3-ubyte";
     public static final String TEST_LABELS_IDX1 = "numbers/t10k-labels.idx1-ubyte";
+
+    public static final int TRAINING_ITERATIONS_COUNT = 25;
 
     public static void main(String[] args) throws IOException {
         File resourceFolder = new File("MNIST");
@@ -49,12 +53,10 @@ public class TestsMNIST {
 
         AlgoClassification knn = new KNN(trainingImagettes, 3);
 
-        int[] layers = {150, 100};
-        double learningRate = 0.6, errorTarget = 2;
-        TransferFunction tf = new Sigmoide();
-        AlgoClassification mlp = new MLPClassification(trainingImagettes, testImagettes, layers, learningRate, tf, errorTarget);
+        List<AlgoClassification> trainedMlps = createAndTrainMlps(trainingImagettes, testImagettes);
 
-        List<AlgoClassification> algos = List.of(knn, mlp);
+        List<AlgoClassification> algos = new ArrayList<>(trainedMlps);
+        algos.add(knn);
 
         for (AlgoClassification algo : algos) {
             int errorCount = 0;
@@ -70,4 +72,53 @@ public class TestsMNIST {
         }
     }
 
+    private static List<AlgoClassification> createAndTrainMlps(List<Imagette> trainingImagettes, List<Imagette> testImagettes) {
+        List<AlgoClassification> mlps = new ArrayList<>();
+        List<int[]> possibleLayers = List.of(
+                new int[]{100, 100, 100},
+                new int[]{100, 200},
+                new int[]{300},
+                new int[]{150, 100},
+                new int[]{30, 10},
+                new int[]{300, 100},
+                new int[]{50, 100},
+                new int[]{150, 100}
+        );
+        double initialLearningRate = 0.6, finalLearningRate = 0.2;
+        TransferFunction tf = new Sigmoide();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(possibleLayers.size());
+        List<Future<AlgoClassification>> futures = new ArrayList<>();
+
+        for (int[] layers : possibleLayers) {
+            Callable<AlgoClassification> task = () -> {
+                MLPClassification mlp = new MLPClassification(trainingImagettes, TRAINING_ITERATIONS_COUNT, testImagettes, layers,
+                        initialLearningRate, finalLearningRate, tf);
+
+                LearningStatistics stats = mlp.train();
+                CSVExporter exporter = new CSVExporter(stats);
+                try {
+                    exporter.export("statistics/");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return mlp;
+            };
+
+            futures.add(executorService.submit(task));
+        }
+
+        executorService.shutdown();
+
+        for (Future<AlgoClassification> future : futures) {
+            try {
+                mlps.add(future.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return mlps;
+    }
 }
